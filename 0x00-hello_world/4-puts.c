7b674e3e1296d1d9009d1cbe26afd37e6a9201a3
Blackie <mail@gmail.com>
2023-02-18 06:17:46 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,10 @@
  */
 
 
-int main (void)
+int main(void)
 {
 
-	puts("/"Programming is like building a multilingual puzzle);
+puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
 
